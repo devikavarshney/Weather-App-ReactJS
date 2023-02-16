4f415967975dc0bc7edefabf7affe025abb9a9f5
devika <devikavarshney0003@gmail.com>
2023-02-16 22:36:37 +0530

    added input search box, place, weather and also applied background

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBackg from './assets/hot.jpg';
+import coldBackg from './assets/cold.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="app" style={{backgroundImage:`url(${coldBackg})`}}>
+    <div className="overlay">
+      <div className="container">
+        <div className="section section__inputs">
+          <input type="text" name="city" placeholder="Enter the city"/>
+          <button>°F</button>
+        </div>
+        <div className="section section__temperature">
+          <div className="icon">
+            <h3>
+              Moradabad
+            </h3>
+            <img src="" alt="weatherIcon" />
+            <h3>Cloudy</h3>
+          </div>
+          <div className="temperature">
+            <h1>45 °C</h1>
+          </div>
+        </div>
+        {/*bottom description*/}
+      </div>
     </div>
+  </div>
   );
 }
 
