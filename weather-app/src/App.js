6569e75da89985d2ad55c43467590b7504f02b1d
devika <devikavarshney0003@gmail.com>
2023-02-17 02:58:50 +0530

    added api_key and fetched the added, also changed data on website according to the place name

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,34 +1,45 @@
-import hotBackg from './assets/hot.jpg';
-import coldBackg from './assets/cold.jpg';
-import cloud from './assets/cloud.webp';
-import Description from './components/Description';
+import hotBackg from "./assets/hot.jpg";
+import coldBackg from "./assets/cold.jpg";
+import cloud from "./assets/cloud.webp";
+import Description from "./components/Description";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
 
 function App() {
+  const [current, setWeather] = useState(null);
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData("moradabad");
+      setWeather(data);
+    };
+    fetchWeatherData();
+  }, []);
+
   return (
-  <div className="app" style={{backgroundImage:`url(${coldBackg})`}}>
-    <div className="overlay">
-      <div className="container">
-        <div className="section section__inputs">
-          <input type="text" name="city" placeholder="Enter the city"/>
-          <button>°F</button>
-        </div>
-        <div className="section section__temperature">
-          <div className="icon">
-            <h3>
-              Moradabad
-            </h3>
-            <img src={cloud} alt="weatherIcon" />
-            <h3>Cloudy</h3>
+    <div className="app" style={{ backgroundImage: `url(${coldBackg})` }}>
+      <div className="overlay">
+        {current && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name="city" placeholder="Enter the city" />
+              <button>°F</button>
+            </div>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${current.name}, ${current.country}`}</h3>
+                <img src={current.iconURL} alt="weatherIcon" />
+                <h3>{`${current.text}`}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${current.temp_c}`}°C</h1>
+              </div>
+            </div>
+            {/*bottom description*/}
+            <Description current={current} />
           </div>
-          <div className="temperature">
-            <h1>45 °C</h1>
-          </div>
-        </div>
-        {/*bottom description*/}
-        <Description />
+        )}
       </div>
     </div>
-  </div>
   );
 }
 
---
+++ weather-app/src/weatherService.js
@@ -0,0 +1,24 @@
+const API_KEY = "2bd4968948c84508b5f162821231602";
+const makeIconURL = (iconID) => `https:${iconID}`;
+
+const getFormattedWeatherData = async (city) => {
+  const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+  const {
+    current: { feelslike_c, humidity, temp_c, wind_kph, condition:{text,icon}, vis_km,pressure_mb},
+    location: { name, country },
+  } = data;
+  return {
+    name,text,
+    feelslike_c,
+    iconURL : makeIconURL(icon),
+    humidity,
+    temp_c,
+    wind_kph,
+    country,vis_km,pressure_mb
+  };
+};
+
+export { getFormattedWeatherData };
